4e3d567ee9ee48d17b397e3709d45f7efb80bbd3
Jun Guo <gjuoun@gmail.com>
2022-11-25 02:24:40 -0400

    tried build

---
+++ workbox-build/injectManifest-vanilla/public/index.js
@@ -0,0 +1,19 @@
+const params = new URLSearchParams(window.location.search);
+const from = params.get("source") || "browser";
+const el = document.querySelector("#root");
+el.textContent = `Hello from ${from}!`;
+
+if ("serviceWorker" in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register("/sw.js").then(
+    (registration) => {
+      console.log("Service worker registration succeeded:", registration);
+    },
+    /*catch*/ (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error("Service workers are not supported.");
+}
---
+++ workbox-build/injectManifest-vanilla/sw-generate.js
@@ -0,0 +1,22 @@
+// Inside of build.js:
+const { injectManifest } = require("workbox-build");
+
+// These are some common options, and not all are required.
+// Consult the docs for more info.
+injectManifest({
+  globDirectory: "./public",
+  globPatterns: ["**/*.js"],
+  swDest: "public/sw.js",
+  swSrc: "src/sw.js",
+}).then(({ count, size, warnings }) => {
+  if (warnings.length > 0) {
+    console.warn(
+      "Warnings encountered while injecting the manifest:",
+      warnings.join("\n")
+    );
+  }
+
+  console.log(
+    `Injected a manifest which will precache ${count} files, totaling ${size} bytes.`
+  );
+});
